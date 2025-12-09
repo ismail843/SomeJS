f5cbfea8134c72c59075c51d80452fc2197bbb97
ismail Nadaf <ismailnadaf843@gmail.com>
2025-12-09 22:31:38 +0530

    this is about my practices

---
+++ loops/loops1.js
@@ -0,0 +1,159 @@
+// easy levels
+
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i)
+// }
+
+// for (let i = 1; i <21; i++){
+//     if(i%10 === 0){
+//         console.log(i);
+
+//     }
+// }
+
+// for (let i = 10; i>1; i--){
+//     if(i%5 === 0){
+//         console.error(`${2} ${i} = ${2 * i}`);
+
+//     }
+
+// }
+// for (let i = 0; i <= 30; i++){
+//     if(i%2 === 0){
+//         console.log(`${i} - even`);
+
+//     }else{
+//         // console.log("this is an odd ", i);
+//         console.log(`${i} - odd`);
+
+//     }
+// }
+
+// let num = parseInt(prompt("enter the number "));
+// if(num === 12){
+//     console.log(`${num} - this is num`);
+
+// }else{
+//     console.log(`${num} - this is not num`);
+
+// }
+
+// let num = parseInt(prompt("enter the number "));
+//  if (num >= 0){
+
+//      console.log("num is positive");
+// }else{
+//      console.log("num is negative");
+
+//  }
+
+// let age = prompt("");
+
+// if (age === null) {
+//     console.error("enter the num");
+// } 
+// else if (age.trim() === "") {
+//     console.error("good");
+// } 
+// else {
+//     age = Number(age.trim()); // convert string to number
+
+//     if (isNaN(age)) {
+//         console.error("bhai number dedo");
+//     } else {
+//         console.log("congrats");
+//     }
+// }
+
+// let age = prompt("hlo how old you are");
+
+// if (age === null) {
+//     console.error("enter valid number");
+// } 
+// else if (age.trim() === "") {
+//     console.error("plz enter some number bro");
+// } 
+// else {
+//     age = Number(age.trim());
+    
+//     if (isNaN(age)) {
+//         console.error("bhai number dedo yaar");
+//     } else {
+//         console.log("confirm ye number");
+//     }
+// }
+
+
+// let age = prompt("use ki age bataoo");
+
+// if (age === null) {
+//     console.error("u cancelled");
+// } else {
+//     if (age.trim() === "") {
+//         console.error("enter something");
+//     } else {
+//         age = Number(age);
+
+//         if (isNaN(age)) {
+//             console.error("enter the num atleast");
+//         } else {
+//             if (age <= 0) {
+//                 console.log("enter the valid age");
+//             } 
+//             else if (age >= 18) {
+//                 console.log("hey u are eligible");
+//             } 
+//             else {
+//                 console.log("you are not eligible");
+//             }
+//         }
+//     }
+// }
+
+
+// for (let i = 1; i  < 11; i ++){
+//     console.log(`5 x ${i} = ${5 * i}`);
+    
+// }
+
+// for(let i = 11 ; i>1; i--){
+//     console.log(`5 x ${i} = ${5 * i}`);
+    
+// }
+
+// let num = Number(prompt("entr the number"))
+// let len = Number(prompt("enter the number"))
+
+
+// for( i = 0 ; i < len; i ++ ){
+//     console.log(`${num} x ${i} = ${num * i} `);
+    
+// }
+// let count = 0;
+
+// for (let i = 1; i<=16; i++){
+//     if(i>8){
+//        count++;
+//        console.log("this is was runned ");
+       
+//     }
+// }
+
+// console.log(count);
+
+// let pwd = "ismail"
+// let pass = prompt("enter the pwd")
+
+// if(pwd === null){
+//     console.error("dont be null");
+    
+// }else{
+//     if(pwd === pass ){
+//         console.log("good pwd matched ");
+        
+//     }else{
+//         console.log('pwd not matched');
+        
+//     }
+// }
---
+++ loops/loops2.js
@@ -0,0 +1,72 @@
+// let attempts = 0;
+// let pass = "smile"
+
+// let pwd = prompt("enter the pwd")
+// attempts++;
+
+// while(pass !== pwd){
+//     if(attempts === 3){
+//         console.error("account blocked");
+//         break;
+
+//     }
+//     pwd = prompt("pwd bolo")
+//     attempts++;
+// }
+
+
+// let words = prompt("enter the word ")
+// let count = 0;
+
+// while(words !== "stop"){
+//     if(words === "yes") count++;
+//     words= prompt("reneter")
+// }
+// console.log(`total time ${count}`);
+
+// for (let i = 1; i< 51; i++){
+//  if(i%7 === 0){
+//     console.log(`the div ${i}`);
+
+//  }
+// }
+
+// let sum = 0;
+
+// for(let i= 1; i < 31;i++){
+//     if(i%2 !== 0){
+//         // console.log(i);
+//         sum = sum + i;
+//         console.log(sum);
+
+//     }
+// }
+
+// let pass = prompt("num bolo")
+// pass = Number(pass)
+
+// while(pass % 2 !== 0){
+//     pass = prompt("give number ")
+//     pass = Number(pass)
+
+// }
+// console.log(`yes number ${pass} `);
+
+// let start = prompt("emter the number ")
+// let end = prompt ("enter the last number ")
+// start = Number(start)
+// end = Number(end)
+//  if (start>end)
+// for (i=start; i <= end; i++){
+//     console.log(i);
+
+// }
+// let counter = 0;
+// for (let i = 0; i < 21; i++) {
+//   if (counter === 3) break;
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//     counter++;
+//   }
+// }
+
